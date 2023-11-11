fad145e006f003a451dc842f5116eccb4b5b3e05
Atlas8304 <johnboulding@outlook.com>
2023-11-11 14:46:30 +0000

    Added auth stuff

--- app/userauth/views.py
+++ app/userauth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
 
@@ -19,3 +20,32 @@ def register_request(request):
         template_name="userauth/register.html",
         context={"register_form": form},
     )
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("blog")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(
+        request=request,
+        template_name="userauth/login.html",
+        context={"login_form": form},
+    )
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("blog")
