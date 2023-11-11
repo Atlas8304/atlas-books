5a710c9bd2915bd38742d2ff08b538e8bc84d2ee
Atlas8304 <johnboulding@outlook.com>
2023-11-11 16:05:41 +0000

    Create Post

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("blog/", views.Post.as_view(), name="blog"),
     path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+	path("blog/new", views.new_post, name="post_new"),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,9 @@
 from django.views import generic
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import BlogPost
+from .forms import NewPostForm
 
 
 class Post(generic.ListView):
@@ -10,3 +14,23 @@ class Post(generic.ListView):
 class PostDetail(generic.DetailView):
     model = BlogPost
     template_name = "blog/post_focused.html"
+
+
+@login_required(login_url="/login")
+def new_post(request):
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.author = request.user
+            obj.slug = obj.title.replace(' ', '-').lower()
+            # TODO: Remove after adding Draft review/publish capability
+            obj.published_state = 1
+            obj.save()
+            return redirect("blog")
+    form = NewPostForm()
+    return render(
+        request=request,
+        template_name="blog/post_new.html",
+        context={"new_post_form": form},
+    )
