27b1c2fd25dc367e4297b5fa8a899126d73e3453
Atlas8304 <johnboulding@outlook.com>
2023-11-11 14:32:28 +0000

    Crispy form Fix + Register

--- app/django_project/settings.py
+++ app/django_project/settings.py
@@ -38,10 +38,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_forms",
+	"crispy_forms",
+	"crispy_bootstrap4",
     "blog",
     "userauth",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -72,7 +77,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "django_project.wsgi.application"
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
