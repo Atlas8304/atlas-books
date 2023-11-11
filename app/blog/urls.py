bf64348c01d5be76ec2f36fab51aa6b63261a667
Atlas8304 <johnboulding@outlook.com>
2023-11-11 16:07:37 +0000

    Added temp home location

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+	# TODO: Remove when proper Splash Page Added
+	path("", views.Post.as_view(), name="blog"),
     path("blog/", views.Post.as_view(), name="blog"),
     path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 	path("blog/new", views.new_post, name="post_new"),
